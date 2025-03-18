e50cdb98304b5cb3c643bbdb3b51144185dfe068
rodrigog02 <zs23013937@estudiantes.uv.mx>
2025-03-18 11:31:31 -0600

    Create ejercicio3.py

---
+++ ejercicio3.py
@@ -0,0 +1,47 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+def fx(x):
+    return np.cbrt(4 + x**3)
+a = 0
+b = 10
+n = 5
+
+h= (b-a)/n
+xi = a
+suma = fx(xi)
+for i in range(0,n-1,1):
+    xi = xi + h
+    suma = suma + 2*fx(xi)
+suma = suma + fx(b)
+area = h*(suma/2)    
+
+print('trapecios:', n)
+print('integral:', area)
+
+
+#GRAFICA
+#puntos de miuestra
+muestras = n + 1
+xi = np.linspace(a,b,muestras)
+fi = fx(xi)
+# linea suave
+muestraslinea= n*10 + 1
+xk = np.linspace(a,b,muestraslinea)
+fk = fx(xk)
+# graficando
+plt.plot(xk,fk, label = 'f(x')
+plt.plot(xi,fi, marker='o',
+         color='orange', label = 'muestras')
+
+plt.xlabel('x')
+plt.ylabel('f(x)')
+plt.title('integral: regla de trapecios')
+plt.legend()
+
+# Trapecios
+plt.fill_between(xi,0,fi, color='g')
+for i in range(0,muestras,1):
+    plt.axvline(xi[i], color='w')
+plt.show()    
